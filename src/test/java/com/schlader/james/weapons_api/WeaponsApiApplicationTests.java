36c44e14bb69a2547519ec58c4582ed8efc7982c
James.Schlader <jamesschlader@yahoo.com>
2020-02-01 23:42:28 -0700

    initial commit

---
+++ src/test/java/com/schlader/james/weapons_api/WeaponsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schlader.james.weapons_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeaponsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
